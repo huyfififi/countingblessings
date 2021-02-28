a433ebb44fa65564b29ab9709de2ccf7a498543c
HN <kazukiyoshida96@gmail.com>
2021-02-28 15:33:58 -0800

    :sparkles: Add email functions

--- config.py
+++ config.py
@@ -3,6 +3,17 @@ basefir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    CB_MAIL_SUBJECT_PREFIX = '[CountingBlessings]'
+    CB_MAIL_SENDER = 'CountingBlessings Admin <countingblessings@example.com>'
+    CB_ADMIN = os.environ.get('CB_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
--- countingblessings.py
+++ countingblessings.py
@@ -1,6 +1,16 @@
 import os
 
-from app import create_app
+from flask_migrate import Migrate
+
+from app import create_app, db
+from app.models import User, Follow, Role, Post
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Follow=Follow, Role=Role,
+                Post=Post)
